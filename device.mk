e3b7a75c69ed26f213f4b68a6e63f020c109aba7
fagyi <fagyipda@gmail.com>
2017-04-27 22:12:10 +0200

    pme: update prebuilts which missing - from logcat

--- device.mk
+++ device.mk
@@ -257,6 +257,10 @@ PRODUCT_PACKAGES += \
     power.msm8996 \
     thermal.msm8996
 
+# Prebuilts
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/org.apache.http.legacy.jar:system/framework/org.apache.http.legacy.jar
+
 # Properties
 PRODUCT_PACKAGES += \
     operator-properties

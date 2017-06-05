3f4cdd6f149a33d9fe54e9b4e1c06424d925b266
fagyi <fagyipda@gmail.com>
2017-06-05 19:37:00 +0200

    pme: build mms apk from source

--- device.mk
+++ device.mk
@@ -220,6 +220,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
+# Messaging
+PRODUCT_PACKAGES += \
+    messaging
+
 # NFC
 PRODUCT_PACKAGES += \
     com.android.nfc_extras \

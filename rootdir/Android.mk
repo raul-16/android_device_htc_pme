ec84932ef25b502b695d15d9ddf10e5b5f887f80
fagyi <fagyipda@gmail.com>
2017-08-12 12:13:57 +0200

    pme: Add init.power.sh to manage EAS from marlin - added few adjusment

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -28,11 +28,19 @@ LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.power.sh
+LOCAL_MODULE_TAGS  := optional eng
+LOCAL_MODULE_CLASS := EXECUTABLES
+LOCAL_SRC_FILES    := bin/init.power.sh
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.power.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/init.qcom.power.rc
 LOCAL_MODULE_PATH  := $(TARGET_ROOT_OUT)
+LOCAL_REQUIRED_MODULES := init.power.sh
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)

6db003d8bb71d9b45453870f1a4f0af3d8346eac
fagyi <fagyipda@gmail.com>
2017-10-09 16:40:07 +0200

    pme: GPS update for Oreo

--- gps/loc_api/libloc_api_50001/LocEngAdapter.cpp
+++ gps/loc_api/libloc_api_50001/LocEngAdapter.cpp
@@ -81,7 +81,6 @@ LocEngAdapter::LocEngAdapter(LOC_API_ADAPTER_EVENT_MASK_T mask,
 {
     memset(&mFixCriteria, 0, sizeof(mFixCriteria));
     mFixCriteria.mode = LOC_POSITION_MODE_INVALID;
-    clearGnssSvUsedListData();
     LOC_LOGD("LocEngAdapter created");
 }
 
@@ -195,9 +194,11 @@ void LocEngAdapter::setXtraUserAgent() {
                 fclose(file);
 
                 // remove trailing spaces
-                size_t len = strlen(buf);
-                while (--len >= 0 && isspace(buf[len])) {
-                    buf[len] = '\0';
+                char *s;
+                s = buf + strlen(buf);
+                while (--s >= buf) {
+                    if (!isspace(*s)) break;
+                    *s = 0;
                 }
             }
 

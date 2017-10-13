e2be095df7b4d5a3eb89845049a4fb6f2991f3f0
fagyi <fagyipda@gmail.com>
2017-10-13 13:22:29 +0200

    pme: add overlay-aosip

--- device.mk
+++ device.mk
@@ -21,7 +21,9 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 $(call inherit-product-if-exists, vendor/htc/pme/pme-vendor.mk)
 
 # Overlays
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-aosip
 
 # Screen density
 PRODUCT_AAPT_CONFIG := normal

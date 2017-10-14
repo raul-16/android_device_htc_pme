956b957e99ac961993882c7f22e7006f25e229ae
fagyi <fagyipda@gmail.com>
2017-10-14 18:33:48 +0200

    pme: Add filter_ie for WiFi

--- device.mk
+++ device.mk
@@ -369,6 +369,7 @@ PRODUCT_PACKAGES += \
     wpa_supplicant.conf
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/filter_ie:system/etc/filter_ie \
     $(LOCAL_PATH)/configs/calibration:system/etc/calibration \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:system/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:system/etc/wifi/wpa_supplicant_overlay.conf

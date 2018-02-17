3a4b5a6676b056bf2f7acbdba2c905bd9b6099fb
fagyi <fagyipda@gmail.com>
2018-02-17 11:40:01 +0100

    pme: add back power shim which needed for FP

--- device.mk
+++ device.mk
@@ -347,7 +347,8 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-service-qti \
+    libshim_power
 
 # Powerhint
 PRODUCT_COPY_FILES += \

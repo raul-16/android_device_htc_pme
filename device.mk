04e4726734df503b2dcf814d0df090d4e19eba5b
fagyi <fagyipda@gmail.com>
2018-02-18 18:56:29 +0100

    pme: bump fingerprint service to 2.1 - Also switch radio to 1.1 version

--- device.mk
+++ device.mk
@@ -196,7 +196,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     fingerprint.msm8996 \
-    android.hardware.biometrics.fingerprint@2.0-service
+    android.hardware.biometrics.fingerprint@2.1-service
 
 # FlipFlap
 # PRODUCT_PACKAGES += \

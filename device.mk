e3fa925481ae887f4ef85fb72f5a430383287981
fagyi <fagyipda@gmail.com>
2018-03-12 18:26:24 +0100

    pme: squash revert the last 3 commits for IPACM
    - Bump my dmesg a lot of issue, silent it via removes
    
    Revert "pme: reset ipacm to LA.UM.5.5.r1-05800-8x96.0"
    
    This reverts commit d766f68bc5520d17302ad77cd6688cbdba6ee2cf.
    
    Revert "pme: Build IPACM"
    
    This reverts commit 6aeb5d6a9e2506d3ea8b86dd5958ccc8cf5fd46a.
    
    Revert "pme: Import IPACM"
    
    This reverts commit c48e5b062db995d28dadea1ae9fc7ad5526c33ac.

--- device.mk
+++ device.mk
@@ -431,8 +431,6 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    ipacm \
-    IPACM_cfg.xml \
     libwpa_client \
     hostapd \
     wificond \

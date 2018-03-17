215f646ca9b42587d830782673747d6912232249
fagyi <fagyipda@gmail.com>
2018-03-17 18:15:10 +0100

    pme: track soundtrigger files to /system/etc too - to fix these errors:
    03-17 17:39:14.713   661  1033 E audio_route: Control 'LSM1 MUX' doesn't exist - skipping
    03-17 17:39:14.713   661  1033 E audio_route: Control 'LSM2 MUX' doesn't exist - skipping
    03-17 17:39:14.713   661  1033 E audio_route: Control 'LSM3 MUX' doesn't exist - skipping
    03-17 17:39:14.713   661  1033 E audio_route: Control 'LSM4 MUX' doesn't exist - skipping
    03-17 17:39:14.713   661  1033 E audio_route: Control 'LSM5 MUX' doesn't exist - skipping
    03-17 17:39:14.713   661  1033 E audio_route: Control 'LSM6 MUX' doesn't exist - skipping
    03-17 17:39:14.713   661  1033 E audio_route: Control 'LSM7 MUX' doesn't exist - skipping
    03-17 17:39:14.713   661  1033 E audio_route: Control 'LSM8 MUX' doesn't exist - skipping
    03-17 17:39:14.714   661  1033 E audio_route: Control 'QUAT_MI2S_RX Port Mixer AUX_PCM_TX' doesn't exist - skipping

--- device.mk
+++ device.mk
@@ -131,7 +131,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tasha.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tasha.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_mixer_paths_wcd9330.xml \
+    $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/sound_trigger_platform_info.xml
 
 PRODUCT_COPY_FILES += \

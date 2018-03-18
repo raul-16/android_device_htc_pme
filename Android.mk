1430ae28aed9f8ebaa2391d69c16ff1d1732762a
fagyi <fagyipda@gmail.com>
2018-03-18 20:55:17 +0100

    pme: fix widevine symlinks

--- Android.mk
+++ Android.mk
@@ -123,15 +123,15 @@ $(VENUS_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 ALL_DEFAULT_INSTALLED_MODULES += $(VENUS_SYMLINKS)
 
 WIDEVINE_IMAGES := \
-    windevine.b00 windevine.b01 windevine.b02 windevine.b03 windevine.b04 \
-    windevine.b05 windevine.b06 windevine.mdt
+    widevine.b00 widevine.b01 widevine.b02 widevine.b03 widevine.b04 \
+    widevine.b05 widevine.b06 widevine.mdt
 
-WIDEVINE_SYMLINKS := $(addprefix $(TARGET_ROOT_OUT)/system/etc/firmware/,$(notdir $(WIDEVINE_IMAGES)))
+WIDEVINE_SYMLINKS := $(addprefix $(TARGET_OUT_VENDOR)/firmware/,$(notdir $(WIDEVINE_IMAGES)))
 $(WIDEVINE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
 	@echo "WIDEVINE firmware link: $@"
 	@mkdir -p $(dir $@)
 	@rm -rf $@
-	$(hide) ln -sf /vendor/etc/firmware/$(notdir $@) $@
+	$(hide) ln -sf /system/etc/firmware/$(notdir $@) $@
 
 ALL_DEFAULT_INSTALLED_MODULES += $(WIDEVINE_SYMLINKS)
 

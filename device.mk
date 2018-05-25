9e39dd2ceb64f82006d98dfabb49dc3981ac5a06
fagyi <fagyipda@gmail.com>
2018-05-25 22:52:13 +0200

    pme: fixup sensors - move out from hwbinder now - seems to fix the issue which present after reboot or flashing something

--- device.mk
+++ device.mk
@@ -432,8 +432,7 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl
 
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \

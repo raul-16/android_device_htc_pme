adf55c686a51c30d6fb0786e44c86845bbb50e2f
fagyi <fagyipda@gmail.com>
2018-07-17 16:46:02 +0200

    pme: remove unused irq_balance config

--- device.mk
+++ device.mk
@@ -274,10 +274,6 @@ PRODUCT_PACKAGES += \
     ethertypes \
     libebtc
 
-# IRQ
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/msm_irqbalance.conf:$(TARGET_COPY_OUT_VENDOR)/etc/msm_irqbalance.conf
-
 # IRSC
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config

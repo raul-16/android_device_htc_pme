048baa332c4e4964599e2bd0b9febad93027d366
fagyi <fagyipda@gmail.com>
2018-07-24 11:30:50 +0200

    pme: update device.mk to build 1.1 power as well
    Also remove the impl line, because Marlin not enabled it, make it more closest with Marlin!

--- device.mk
+++ device.mk
@@ -356,8 +356,7 @@ PRODUCT_COPY_FILES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-impl \
-    android.hardware.power@1.0-service.pme
+    android.hardware.power@1.1-service.pme
 
 # Powerhint
 PRODUCT_COPY_FILES += \

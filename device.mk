e88ecf2cf8bfaf148d15196bcc2578964e190ee6
fagyi <fagyipda@gmail.com>
2018-07-28 16:49:05 +0200

    pme: add PRODUCT_PACKAGES for thermal as well - fixes build

--- device.mk
+++ device.mk
@@ -433,6 +433,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 
 # Thermal
+PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service
 

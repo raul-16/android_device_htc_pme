d660eb2056948d78a13a0010d5bb4abb511cc752
fagyi <fagyipda@gmail.com>
2018-08-23 12:08:16 +0200

    pme: vndk-sp is no vendor module - Also build the vndk_package

--- device.mk
+++ device.mk
@@ -457,6 +457,10 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator@1.0-impl \
     android.hardware.vibrator@1.0-service
 
+# VNDK
+PRODUCT_PACKAGES += \
+    vndk_package
+
 # VNDK-SP:
 PRODUCT_PACKAGES += \
     vndk-sp

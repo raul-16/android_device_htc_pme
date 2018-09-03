fb3daed53e4536c14bc95cf56ca027ae4770e25f
Fagyi <fagyipda@gmail.com>
2018-09-03 09:59:20 +0200

    pme: disable IPACM for bringup - Need update's for get it working on P

--- device.mk
+++ device.mk
@@ -471,8 +471,6 @@ PRODUCT_PACKAGES += \
 # Wifi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service \
-    ipacm \
-    IPACM_cfg.xml \
     libwpa_client \
     hostapd \
     wificond \

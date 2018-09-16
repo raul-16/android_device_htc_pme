3f340f40290ca5556458acebcf8a81d3fc39bceb
fagyi <fagyipda@gmail.com>
2018-09-16 19:43:21 +0200

    pme: Set shipping API level via inherited makefile

--- lineage_pme.mk
+++ lineage_pme.mk
@@ -19,6 +19,7 @@ $(call inherit-product, device/htc/pme/device.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_m.mk)
 
 # Inherit some common Lineage stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)

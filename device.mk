44b7374c02ab5381042ceb7b9aaec3a3c930ef8b
fagyi <fagyipda@gmail.com>
2018-10-23 14:40:39 +0200

    Revert "pme: sensors: move to hwbinder"
    This reverts commit b3e97edc6ec7d650258777c7979b655f65241c7f.

--- device.mk
+++ device.mk
@@ -422,8 +422,7 @@ PRODUCT_COPY_FILES += \
 # Sensors
 PRODUCT_PACKAGES += \
     libsensorndkbridge \
-    android.hardware.sensors@1.0-impl \
-    android.hardware.sensors@1.0-service
+    android.hardware.sensors@1.0-impl
 
 # Tethering
 PRODUCT_PROPERTY_OVERRIDES += \

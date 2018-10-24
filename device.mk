fd720b227e81ebaa024a8a6856dce28612e36ab2
Fagyi <fagyipda@gmail.com>
2018-10-24 17:01:36 +0200

    pme: remove thermal service's for now

--- device.mk
+++ device.mk
@@ -446,12 +446,7 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
-# Thermal HAL
-PRODUCT_PACKAGES += \
-    android.hardware.thermal@1.0-impl \
-    android.hardware.thermal@1.0-service \
-    thermal.pme
-
+# Thermal
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 

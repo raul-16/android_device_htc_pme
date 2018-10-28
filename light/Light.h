c72911c5717386c6a9cc6d3a420abf45c78e8fe6
fagyi <fagyipda@gmail.com>
2018-10-28 13:28:18 +0100

    pme: Sync Light interfaces with P

--- light/Light.h
+++ light/Light.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2016 The Android Open Source Project
+ * Copyright (C) 2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #ifndef ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
 #define ANDROID_HARDWARE_LIGHT_V2_0_LIGHT_H
 
@@ -56,6 +57,7 @@ private:
     std::unordered_map<Type, std::function<void(const LightState&)>> mLights;
     std::mutex mLock;
 };
+
 }  // namespace implementation
 }  // namespace V2_0
 }  // namespace light

af2bb2eb1a175eff5af35136aa2f5292898390a7
fagyi <fagyipda@gmail.com>
2018-10-31 11:16:16 +0100

    pme: remove and cleanup unused prebuilt - Make it more closest for official

--- device.mk
+++ device.mk
@@ -98,10 +98,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries_system.txt:system/etc/public.libraries.txt \
     $(LOCAL_PATH)/configs/public.libraries_vendor.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
 
-# Adblocker
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/adblocker:system/bin/adblocker
-
 # ANT+
 PRODUCT_PACKAGES += \
     AntHalService \

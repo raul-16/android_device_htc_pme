81fb8946fd9e099b9193c085ffeb8c8bb008e117
Fagyi <fagyipda@gmail.com>
2018-11-29 16:19:31 +0100

    pme: cleanup drm service - Remove android.hardware.drm@1.0-service.widevine because this is a BLOB.

--- device.mk
+++ device.mk
@@ -209,9 +209,8 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-service \
-    android.hardware.drm@1.0-service.widevine \
-    android.hardware.drm@1.0-impl
+    android.hardware.drm@1.0-impl \
+    android.hardware.drm@1.0-service
 
 # Fingerprint
 PRODUCT_PACKAGES += \

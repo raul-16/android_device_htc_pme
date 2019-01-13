6bb0cc089566f9bba0140e09f7bef1b509d27f08
fagyi <fagyipda@gmail.com>
2019-01-13 15:42:37 +0100

    pme: thermal cleanup's

--- device.mk
+++ device.mk
@@ -451,6 +451,11 @@ PRODUCT_PACKAGES += \
     textclassifier.bundle1
 
 # Thermal
+PRODUCT_PACKAGES += \
+    thermal.msm8996 \
+    android.hardware.thermal@1.0-impl \
+    android.hardware.thermal@1.0-service
+
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
 

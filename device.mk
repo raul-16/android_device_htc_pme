d8a93b009ca232522d66567257c2e18f1552037c
fagyi <fagyipda@gmail.com>
2019-02-16 21:28:46 +0100

    Revert "pme: Migrate to livedisplay 2.0"
    This reverts commit e6baeb7859f1fa7e9c11542601d5648341465e74.

--- device.mk
+++ device.mk
@@ -324,7 +324,7 @@ PRODUCT_PACKAGES += \
 
 # LiveDisplay
 PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.0-service.pme
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Marlin IRQ Balancer configuration file
 PRODUCT_COPY_FILES += \

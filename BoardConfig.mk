71b47d5503b7098b306488faa9312b365524b1c6
fagyi <fagyipda@gmail.com>
2019-02-18 17:17:02 +0100

    pme: WIP disable device specific sepolicy - TODO: Need fix neverallows

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -204,7 +204,7 @@ TARGET_RIL_VARIANT := caf
 
 # SELinux
 include device/qcom/sepolicy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
+# BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
 # SHIMS
 TARGET_LD_SHIM_LIBS := \

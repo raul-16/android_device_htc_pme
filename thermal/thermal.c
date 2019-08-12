31db9f07709702c6fd746e043fe6a2f4db7e9d9e
fagyi <fagyipda@gmail.com>
2019-08-12 17:31:53 +0200

    Revert "pme: configure thermal for OP3 P thermal blobs"
    This reverts commit 5fd7888feac007e954c2f2f3f3788dce1febcdbb.

--- thermal/thermal.c
+++ thermal/thermal.c
@@ -48,7 +48,7 @@ const int CPU_SENSORS[] = {4, 6, 9, 11};
 #define CPU_THROTTLING_THRESHOLD      95
 #define BATTERY_SHUTDOWN_THRESHOLD    60
 // device/htc/pme/configs/thermal-engine.conf
-#define SKIN_THROTTLING_THRESHOLD     47
+#define SKIN_THROTTLING_THRESHOLD     44
 #define SKIN_SHUTDOWN_THRESHOLD       60
 #define VR_THROTTLED_BELOW_MIN        58
 

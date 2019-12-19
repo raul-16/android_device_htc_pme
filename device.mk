9e7ccdb6eb314bd73a252d375f5741000a11757c
Stephane Lee <stayfan@google.com>
2019-12-19 14:57:04 -0800

    pme: health HAL 2.1
    Bug: b/137790244
    Test: lshal debug (health service)
    
    Change-Id: Id15d203bd791d0867972da98f6998610723b9332

--- device.mk
+++ device.mk
@@ -262,10 +262,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
     $(LOCAL_PATH)/gps/etc/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/xtwifi.conf
+
 # Health
 PRODUCT_PACKAGES += \
-    android.hardware.health@2.0-impl \
-    android.hardware.health@2.0-service
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-service
 
 # HIDL
 PRODUCT_PACKAGES += \

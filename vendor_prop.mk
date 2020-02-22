6552ea0a73059043f1cff170eafd17293e78ea4e
Fagyi <fagyipda@gmail.com>
2020-02-22 18:30:36 +0100

    pme: Add lmkd props
     * Kang lmkd props from newest google devices (aka Pixels).
    
     * This makes lmkd spread its wings and become a multitasking beast over kernel side LMK.

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -208,6 +208,18 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.qti.sensors.sta_detect=false \
     ro.qti.sensors.mot_detect=false
 
+# Set lmkd options
+PRODUCT_PRODUCT_PROPERTIES += \
+    ro.lmk.low=1001 \
+    ro.lmk.medium=800 \
+    ro.lmk.critical=0 \
+    ro.lmk.critical_upgrade=false \
+    ro.lmk.upgrade_pressure=100 \
+    ro.lmk.downgrade_pressure=100 \
+    ro.lmk.kill_heaviest_task=true \
+    ro.lmk.kill_timeout_ms=100 \
+    ro.lmk.use_minfree_levels=true
+
 # Surfaceflinger
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.early_phase_offset_ns=1500000 \

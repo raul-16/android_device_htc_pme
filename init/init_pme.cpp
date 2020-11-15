b106062cce35e5ac33cfd7861d016378ea438c83
Fagyi <fagyipda@gmail.com>
2020-11-15 10:26:27 +0100

    pme: init: Fix compilation on R

--- init/init_pme.cpp
+++ init/init_pme.cpp
@@ -30,9 +30,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
-#include <sys/_system_properties.h>
-
 #include <android-base/properties.h>
 #include <android-base/logging.h>
 #include "vendor_init.h"
@@ -52,6 +49,7 @@
 
 #define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
 #include <sys/_system_properties.h>
+using android::base::GetProperty;
 
 void property_override(char const prop[], char const value[], bool add = true)
 {
@@ -62,17 +60,6 @@ void property_override(char const prop[], char const value[], bool add = true)
     } else if (add) {
         __system_property_add(prop, strlen(prop), value, strlen(value));
     }
-}
-
-void property_override(char const prop[], char const value[])
-{
-    prop_info *pi;
-
-    pi = (prop_info*) __system_property_find(prop);
-    if (pi)
-        __system_property_update(pi, value, strlen(value));
-    else
-        __system_property_add(prop, strlen(prop), value, strlen(value));
 }
 
 void property_override_dual(char const system_prop[], char const vendor_prop[],

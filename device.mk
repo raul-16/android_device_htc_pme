84a29c53b504e983b82fb35b455e5471284637c3
raul16 <cristian.spataru@gmail.com>
2021-02-19 15:45:49 +0200

    pme: Switch to source built vendor.qti.hardware.perf@1.0.so
    For some reason, any prebuilt ones from pre-R ROMs no longer work on R
    and cause the following error:
      E ANDR-PERF: Unable to link to gPerfHal death notifications!
    What's worse, the minimal CPU frequency of both clusters will be locked
    at 1036MHz on msm8996.
    
    Author:     dianlujitao <dianlujitao@lineageos.org>
    AuthorDate: 2021-02-09 18:14:04 +0800

--- device.mk
+++ device.mk
@@ -422,7 +422,9 @@ PRODUCT_PACKAGES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
-    android.hardware.power.stats@1.0-service.mock
+    android.hardware.power.stats@1.0-service.mock \
+    vendor.qti.hardware.perf@1.0.vendor
+
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml

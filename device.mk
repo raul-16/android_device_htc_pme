eba3bf2c5d26919ec51f53696e1dddb0e478ce93
raul16 <cristian.spataru@gmail.com>
2021-02-21 21:20:04 +0200

    pme: cleanup 890204

--- device.mk
+++ device.mk
@@ -423,7 +423,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@1.0.vendor
+    vendor.qti.hardware.perf@1.0
 
 
 PRODUCT_COPY_FILES += \

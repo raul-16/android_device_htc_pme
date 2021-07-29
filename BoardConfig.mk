d59cc1f97c76488a2d9fdd09bd9ee2e485014d65
raul16 <cristian.spataru@gmail.com>
2021-07-29 21:08:57 +0300

    pme: update fstab as per stock

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -187,7 +187,7 @@ DEVICE_MATRIX_FILE   := $(DEVICE_PATH)/compatibility_matrix.xml
 HWUI_COMPILE_FOR_PERF := true
 
 # Init
-TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
+#TARGET_PLATFORM_DEVICE_BASE := /devices/soc.0/
 TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_pme
 TARGET_RECOVERY_DEVICE_MODULES := libinit_pme
 

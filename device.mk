6c722f3e1bf7a1d60ca95e5ed50add8615593727
raul16 <cristian.spataru@gmail.com>
2022-02-27 14:52:35 +0200

    pme: build libaudiopolicymanager

--- device.mk
+++ device.mk
@@ -110,10 +110,11 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usb.default \
     libaudio-resampler \
-    libaudioroute \
+    libaudiopolicymanager \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libaudioroute \
     libvolumelistener \
     tinymix
 

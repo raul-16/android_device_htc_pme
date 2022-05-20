c653343b65995e87e666fd57f81ad29a23fc4c70
raul16 <cristian.spataru@gmail.com>
2022-05-20 19:13:06 +0300

    pme: Remove deprecated TARGET_USES_QTI_CAMERA_DEVICE
    * qcom-camera topic hasn't been ported to 19.1
    * Dead as of https://review.lineageos.org/c/LineageOS/android_vendor_lineage/+/331507.
    * We have to build vendor.qti.hardware.camera.device@1.0 interface lib as IMS stack and possibly camera HAL still needs it

--- device.mk
+++ device.mk
@@ -162,6 +162,7 @@ PRODUCT_COPY_FILES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    vendor.qti.hardware.camera.device@1.0.vendor \
     com.qualcomm.qti.camera \
     libmm-qcamera \
     libshim_camera \

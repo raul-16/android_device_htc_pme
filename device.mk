320b353402a6f61367a811a7031ab81b1dac6a06
raul16 <cristian.spataru@gmail.com>
2022-10-15 13:29:43 +0300

    pme: Build Bluetooth audio A2DP
    requires Revert "Set Bluetooth apex updatable to true"

--- device.mk
+++ device.mk
@@ -153,6 +153,7 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.bluetooth@1.0-impl \
     android.hardware.bluetooth@1.0-service \
+    audio.a2dp.default \
     libbt-vendor
 
 PRODUCT_COPY_FILES += \
